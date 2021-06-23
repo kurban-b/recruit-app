fd166ca63929013bb0d737de92860eb3736cabe4
Kurban <bdobik@mail.ru>
2021-06-23 20:36:50 +0300

    выгрузка на хероку попытка 3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults({
-  static: "/build"
+  static: "./build"
 });
 
 const recruiters = router.db.get("recruiters");
